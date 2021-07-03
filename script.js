1ce5cab5e4897f6286909c0f3f96549ebaf8b97a
gthbskate <skate.boarding@live.com.mx>
2021-07-03 15:04:18 -0500

    2da parte

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ loadAsset("./shrek.jpg", "blob", displaymage);*/
     console.log("fetch problem: " + err.message);
 });*/
 
-console.log("Starting");
+/*console.log("Starting");
 let image;
 
 fetch("coffe.jpg").then((response) => {
@@ -94,4 +94,109 @@ fetch("coffe.jpg").then((response) => {
     console.log("There has been a problem with your fetch operation: " + error.message);
 });
 
-console.log("All done");
+console.log("All done");*/
+
+/*let myGreating = setTimeout(function() {
+    alert("Hello MR. Jhonson");
+}, 2000)
+
+function sayHi(who) {
+    alert(`Hello Mr. Universe ${who}!`);
+}
+
+let myGreatingt = setTimeout(sayHi, 2000, "Sr. Misterio");
+
+clearTimeout(myGreating);*/
+
+/*function displayTime() {
+    let date = new Date();
+    let time = date.toLocaleTimeString();
+    document.querySelector(".clock").textContent = time;
+}
+
+displayTime();
+const createClock = setInterval(displayTime, 1000);
+
+clearInterval(createClock);*/
+
+/*let i = 1;*/
+
+/*setTimeout(function run() {
+    console.log(i);
+    i++;
+    setTimeout(run, 100);
+}, 100);*/
+
+/*setInterval(function run (){
+    console.log(i);
+    i++
+}, 100);*/
+
+/*setTimeout(function() {
+    alert("world");
+}, 0);
+
+alert("Hello");*/
+
+/*choiseTopings()
+.then(function(topings){
+    return placeOrder(topings);
+})
+.then(function(order){
+    return collectOrder(order);
+})
+.then(function(pizza){
+    eatPizza(pizza);
+})
+.catch(falureCallback);*/
+
+/*choiseTopings()
+.then(toppings =>
+    placeOrder(toppings)
+)
+.then(order =>
+    collectOrder(order)
+)
+.then(pizza =>
+    eatpizza(pizza)
+)
+.catch(failureCallback);*/
+
+/*choiseTopings()
+.then(toppings => placeOrder(toppings))
+.then(order => collectOrder(order))
+.then(pizza => eatpizza(pizza))
+.catch(failureCallback);*/
+
+/*choiceToppings().then(placeOrder).then(collectorder).then(eatPIzza).catch(failureCallback);*/
+
+/*let promise = fetch("coffee.jpg");
+
+let promise2 = promise.then(response => response.blob());
+
+let promise3 = promise2.then(myBlob => {
+    let objectURL = URL.createObjectURL(myBlob);
+    let image = document.createElement("img");
+    image.src = objectURL;
+    document.body.appendChild(image);
+})
+
+let errorCase = promise3.catch(e => {
+    console.log("There has been a problem with your fetch operation: " + e.message);
+});*/
+
+fetch("coffee.jpg")
+
+.then(response => response.blob())
+
+.then(myBlob => {
+    let objectURL = URL.createObjectURL(myBlob);
+
+    let image = document.createElement("img");
+
+    image.src = objectURL;
+
+    document.body.appendChild(image);
+}).catch(e => {
+    console.log("There has been a problem with your fetch operation: " + e.mesage);
+});
